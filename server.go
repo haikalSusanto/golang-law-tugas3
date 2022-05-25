c947354e5c8aa8000856ce0057284bf4dc90862c
Muhammad Haikal Ubaidillah Susanto <haikalsusanto21@gmail.com>
2022-05-25 17:31:53 +0700

    return html

--- server.go
+++ server.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"crypto/tls"
+	"html/template"
+	"io"
 	"net/http"
 
 	logrustash "github.com/bshuster-repo/logrus-logstash-hook"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 )
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	log := logrus.New()
@@ -22,10 +39,18 @@ func main() {
 	})
 
 	e := echo.New()
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+	e.Renderer = renderer
+
 	e.GET("/", func(c echo.Context) error {
 		param := c.QueryParam("param")
 		logger.Info(param)
-		return c.String(http.StatusOK, ("param: " + param))
+		return c.Render(http.StatusOK, "home.html", map[string]interface{}{
+			"param": param,
+		})
 	})
 	e.Start(":" + "5433")
 }
